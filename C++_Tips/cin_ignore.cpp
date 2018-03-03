0e42634daf70c46a21c56efc3d84f140ee1c7b8c
Wiktor Bednarek <200190@student.pwr.edu.pl>
2018-03-03 03:12:26 +0100

    cin.ignore with C++ style max int value added.

--- C++_Tips/cin_ignore.cpp
+++ C++_Tips/cin_ignore.cpp
@@ -1,1 +1,4 @@
 cin.ignore(INT_MAX, '\n'); //Skips number of INT_MAX symbols to the next line.
+
+
+cin.ignore(numeric_limits<int>::max(), '\n'); //Skips number of INT_MAX symbols to the next line. C++ style.
