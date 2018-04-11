70ad912bf3513b8fd3259cbce1eed1e60b00ec37
Wiktor Bednarek <200190@student.pwr.edu.pl>
2018-04-11 03:01:56 +0200

    On enter pressed activity added. FINALLY I know how to do something when ENTER pressed (\n sign found!

---
+++ C++_Tips/ENTER_pressed_activity.cpp
@@ -0,0 +1,56 @@
+//!!! CRUCIAL LINE if (cin.get() == '\n') 
+
+#include "stdafx.h"
+#include<iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+
+void non_a_number()
+{
+	if (cin.fail())
+	{
+		cin.clear();
+		cout << "To nie by³a liczba" << endl;
+		cout << "Spróbuj ponownie" << endl;
+		char ch;
+		while (cin >> ch)
+		{
+			if (isdigit(ch))
+			{
+				cin.unget();
+				break;
+			}
+			if (cin.get() == '\n') //!!! CRUCIAL LINE
+			{
+				cout << "Wcia¿ to nie jest liczba" << endl;
+			}
+			cin.unget();
+		}
+	}
+}
+
+
+int main()
+{
+	cout << "Wprowadz jak¹kolwiek liczbê" << endl;
+	int number = -17;
+	while (true)
+	{
+		if (cin >> number)
+		{
+			cout << "Wprowadziles liczbê: " << number << endl;
+			cout << "Koniec programu" << endl;
+			break;
+
+		}
+		non_a_number();
+
+	}
+	system("pause");
+
+	return 0;
+}
