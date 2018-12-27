3da1e7e1335d258deca699404fa282c6e2dc62c0
Wiktor Bednarek <200190@student.pwr.edu.pl>
2018-12-27 15:10:50 +0100

    Factorial function added my solution + Bjarne Stroustrup 's solution from Principles and Practice Using C++book.

---
+++ C++_Tips/Factorial.cpp
@@ -0,0 +1,43 @@
+#include<sstream> 
+#include<string>
+#include<stdexcept> 
+#include <iostream>
+
+
+inline void error(const std::string& s)
+{
+	throw std::runtime_error(s);
+}
+
+
+
+long long fct(int n)
+{
+	if (n > 19) error("to big number for long long type, use number less than 20");
+	int num = 1;
+	for (int i = 1; i < n + 1; ++i)
+	{
+		num *= i;
+	}
+
+
+	return num;
+}
+
+int fac_Stroustrup(int n)
+{
+	int r = 1;
+	while (n > 1)
+	{
+		r *= n;
+		--n;
+	}
+	return r;
+}
+
+
+int main()
+{
+	std::cout << fct(15) << std::endl;
+	std::cout << fac_Stroustrup(15) << std::endl;
+}
