774f4ff44cf67e10b05ddce504dd784a8e94bea1
Wiktor Bednarek <wdbednarek@quinnipiac.edu>
2019-10-15 00:55:56 +0200

    Good GUI example added.

---
+++ C++_Tips/Good_comprehensive_example/Good_GUI_example.h
@@ -0,0 +1,63 @@
+//Example is from Stroustrup PPP book, Chapter 16, Homework 5.
+
+#pragma once
+#include "../Simple_window.h"    // get access to our window library
+#include "../Graph.h"            // get access to our graphics library facilities
+
+namespace H16_5
+{
+
+	class Hexagon : public Polygon
+	{
+	public:
+		Hexagon::Hexagon(Point start, int edge_length);
+		void draw_lines() const;
+		void move(int dx, int dy);
+		void move_to_position(int x, int y);
+		int get_curr_pos_x() { return curr_pos_x; }
+		int get_curr_pos_y() { return curr_pos_y; }
+	private:
+		int curr_pos_x;
+		int curr_pos_y;
+		Point start;
+		Polygon hexagon;
+	};
+
+	class My_window : Window
+	{
+	public:
+		My_window(Point xy, int w, int h, const string& title);
+	private:
+		const int window_width;
+		const int window_height;
+		const int MAX_X_BORDER;
+		const int MAX_Y_BORDER;
+		const int button_height;
+		const int edge_length;
+
+		bool draw_button_pushed;
+		Button draw_figure_button;
+		Button next_position_button;
+		In_box x_coordinate_input;
+		In_box y_coordinate_input;
+		Out_box message_box;
+		Vector_ref<Hexagon> hexagons;
+
+		int get_x_coordinate();
+		int get_y_coordinate();
+
+		void display_coords(int x, int y);
+		void draw_figure();
+		bool is_figure_in_scope(int curr_x, int curr_y);
+		void next_figure_position();
+
+		static void cb_place_hexagon(Address, Address);
+		static void cb_next_figure_position(Address, Address);
+	};
+
+
+
+
+
+}
+
